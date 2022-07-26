0fbb10f38005996ac57faf42611d06d773c2b330
Kinamod <Domanic@auth0.com>
2022-07-26 10:28:49 +0100

    cleaned code, toggle facet not add

--- resources/app.js
+++ resources/app.js
@@ -1,10 +1,7 @@
-// const algoliasearch = require("algoliasearch");
-// const algoliasearchHelper = require("algoliasearch-helper");
-
 const applicationID = "YEF3FUMF7I";
 const apiKey = "7a8595e0981d765c076de998c602ab77";
 const indexName = "restaurantlist";
-const debuggingOn = true;
+const debuggingOn = false;
 
 var client = algoliasearch(applicationID, apiKey);
 // var helper = algoliasearchHelper(client, indexName);
@@ -12,16 +9,15 @@ var helper = algoliasearchHelper(client, indexName, {
   facets: ["food_type", "stars_count"],
   disjunctiveFacets: ["payment_options"],
 });
+
 var currentHitsPerPage = 20;
 
 function init() {
-  $(function () {
-    currentHitsPerPage = 20;
-    helper.setQuery($(this).val()).search();
-    debugLog("loaded");
-  });
+  //initially populate results
+  helper.setQuery($(this).val()).search();
+  debugLog("loaded just now");
 
-  $(".cardIcon").click(function () {
+  $(".cardIcon").on("click", function () {
     $(this).toggleClass("selected");
   });
 
@@ -39,7 +35,6 @@ function init() {
     debugLog(content);
   });
 
-  // helper.search();
   $("#search-box").on("keyup", function () {
     currentHitsPerPage = 20;
     helper
@@ -53,62 +48,24 @@ function init() {
     debugLog(facetValue);
     helper.toggleFacetRefinement("food_type", facetValue).search();
   });
-
-  // $("#showmore").on("click", function () {
-  //   currentHitsPerPage += 10;
-
-  //   debugLog("show more " + currentHitsPerPage);
-  //   helper.setQueryParameter("hitsPerPage", currentHitsPerPage).search();
-  // });
 }
+
 function paymentFilterOption(facetValue) {
   // var facetValue = $(e).data("facet");
-  debugLog(facetValue, true);
-  debugLog(helper.getDisjunctiveRefinements("payment_options"), true);
-  helper.addDisjunctiveFacetRefinement("payment_options", facetValue);
+  debugLog("paymentFilterOption");
+  debugLog(facetValue);
+  debugLog(helper.getRefinements("payment_options"));
+  helper.toggleFacetRefinement("payment_options", facetValue);
   if (facetValue == "Discover") {
     debugLog("this is discover");
-    helper.addDisjunctiveFacetRefinement("payment_options", "Discover");
-    helper.addDisjunctiveFacetRefinement("payment_options", "Diners Club");
-    helper.addDisjunctiveFacetRefinement("payment_options", "Carte Blanche");
+    // helper.toggleFacetRefinement("payment_options", "Discover");
+    helper.toggleFacetRefinement("payment_options", "Diners Club");
+    helper.toggleFacetRefinement("payment_options", "Carte Blanche");
   }
   helper.search();
 }
+
 function filterStars(noStars) {
-  // debugLog(helper.hasRefinements("stars_count"));
-  //DSJ How do I do the rating and sort it?
-
-  // helper
-  //   .toggleFacetRefinement("stars_count", noStars + ".0")
-  //   .toggleFacetRefinement("stars_count", noStars + ".1")
-  //   .toggleFacetRefinement("stars_count", noStars + ".2")
-  //   .toggleFacetRefinement("stars_count", noStars + ".3")
-  //   .toggleFacetRefinement("stars_count", noStars + ".4")
-  //   .toggleFacetRefinement("stars_count", noStars + ".5")
-  //   .toggleFacetRefinement("stars_count", noStars + ".6")
-  //   .toggleFacetRefinement("stars_count", noStars + ".7")
-  //   .toggleFacetRefinement("stars_count", noStars + ".8")
-  //   .toggleFacetRefinement("stars_count", noStars + ".9")
-  //   .search();
-
-  // if (helper.hasRefinements("stars_count")) {
-  //   debugLog(
-  //     "do we already have: " +
-  //       noStars +
-  //       " - " +
-  //       helper.getNumericRefinement("stars_count", ">="),
-  //     true
-  //   );
-  //   if (helper.getNumericRefinement("stars_count", ">=").includes(noStars)) {
-  //     debugLog("we already have: - " + noStars, true);
-  //     helper.removeNumericRefinement("stars_count", ">=", noStars).search();
-  //     debugLog("we have removed: " + noStars, true);
-  //   } else {
-  //     helper.addNumericRefinement("stars_count", ">=", noStars).search();
-  //   }
-  // } else {
-  //   helper.addNumericRefinement("stars_count", ">=", noStars).search();
-  // }
   $("#ratingStarsSection .filterRow").removeClass("selected");
   if (helper.hasRefinements("stars_count")) {
     debugLog("has: " + helper.getNumericRefinement("stars_count", ">="));
@@ -127,29 +84,15 @@ function filterStars(noStars) {
       "selected"
     );
   }
-
-  //set selections
-
-  // for (let i = noStars; i > 0; i--) {
-
-  // }
-
-  // debugLog(helper.hasRefinements("stars_count"));
-
-  // helper.search("query", {
-  //   filters: `stars_count:${noStars} TO ${noStars + 1}`,
-  // });
-  // debugLog("star count filter");
-  // helper.search("query", {
-  //   filters: "stars_count: 4 TO 5",
-  // });
 }
+
 function showMoreFunction() {
   currentHitsPerPage += 10;
 
   debugLog("show more " + currentHitsPerPage);
   helper.setQueryParameter("hitsPerPage", currentHitsPerPage).search();
 }
+
 function renderHits(results) {
   // $("#container").html(JSON.stringify(results, null, 2));
   debugLog(results[0].nbHits);
@@ -181,11 +124,13 @@ function renderHits(results) {
     return " in " + results[0].processingTimeMS / 1000 + " seconds";
   });
 }
+
 const showMore = `
 <button id="showmore" type="button" onclick="showMoreFunction()" class="btn btn-light">
       Show More
     </button>
 `;
+
 function createResultCard(hit) {
   const percent = Math.min((hit.stars_count + 0.1) * 20, 100);
   const tempResultCard = `
@@ -243,33 +188,8 @@ function renderFacetList(content) {
   });
 }
 
-// function renderFacetList(content) {
-//   debugLog("CONTENT facets");
-//   debugLog(content);
-//   debugLog(content.getFacetValues("food_type"));
-//   $("#foodTypeFacet").html(function () {
-//     return $.map(content.getFacetValues("food_type"), function (facet) {
-//       var checkbox = $("<input type=checkbox>")
-//         .data("facet", facet.name)
-//         .attr("id", "fl-" + facet.name);
-//       if (facet.isRefined) {
-//         checkbox.attr("checked", "checked");
-//       }
-//       var label = $("<label>")
-//         .html(facet.name + " (" + facet.count + ")")
-//         .attr("for", "fl-" + facet.name);
-//       return $("<li>").append(checkbox).append(label);
-//     });
-//   });
-// }
-
-$(document).ready(init);
-
-// app.listen(4242, () => debugLog(`Node server listening on port ${4242}!`));
-
 function debugLog(debugmessage, high) {
   if (debuggingOn || high) console.log(debugmessage);
 }
-// function debugLogHigh(debugmessage) {
-//   console.log(debugmessage);
-// }
+
+$(init);
